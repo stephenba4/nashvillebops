9e74a37f683f63d0a874635f6b54ae98e17160d3
Stephen Anthony <stephenba4@gmail.com>
2021-02-14 15:58:52 -0600

    nb5: Add data file to state

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import monthlyListenerData from '@/data/monthlyListenerData';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    monthlyListenerFields: monthlyListenerData.fields,
+    monthlyListenerItems: monthlyListenerData.items,
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    getMonthlyListenerFields(state) {
+      return state.monthlyListenerFields;
+    },
+    getMonthlyListenerItems(state) {
+      return state.monthlyListenerItems;
+    },
   },
 });
