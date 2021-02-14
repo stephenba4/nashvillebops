c6e067a35708ddf3ba152cad9371bb53e3d679c7
Stephen Anthony <stephenba4@gmail.com>
2021-02-13 22:14:17 -0600

    nb1: Mimic table in wireframe on main page with fake data

---
+++ src/components/ArtistTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <b-container>
+      <b-table :items="items" />
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArtistTable',
+  props: {
+    msg: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      items: [
+        { number: 1, artist: 'Nelly', monthlyListeners: 100 },
+        { number: 2, artist: 'Britney Spears', monthlyListeners: 90 },
+        { number: 3, artist: 'Chris Brown', monthlyListeners: 80 },
+      ],
+    };
+  },
+};
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import ArtistTable from '@/components/ArtistTable.vue';
 
-describe('HelloWorld.vue', () => {
+describe('ArtistTable.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(ArtistTable, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
