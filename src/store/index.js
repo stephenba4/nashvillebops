798c90846423058dda303db1cbba575dda68016a
Stephen Anthony <stephenba4@gmail.com>
2021-02-15 19:45:09 -0600

    nb7: capitalization

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import monthlyListenerData from '@/data/monthlyListenerData';
+import monthlyListenerData from '@/data/MonthlyListenerData';
 
 Vue.use(Vuex);
 
