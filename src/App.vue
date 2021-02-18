d830dee547136de82e0af29f977119cfff2a8edc
Stephen Anthony <stephenba4@gmail.com>
2021-02-17 21:11:31 -0600

    nb9: artist count

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,35 @@
       </router-link>
     </b-navbar>
 
+    <b-container>
+      <p class="m-3" style="border-bottom: 1px solid grey">
+        Artists: {{ artistCount }}
+      </p>
+    </b-container>
+
     <router-view />
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      fakeData: {},
+      artistCount: null,
+    }
+  },
+  computed: {
+    ...mapGetters(['getFakeData']),
+  },
+  mounted() {
+    this.fakeData = this.getFakeData;
+    this.artistCount = this.getFakeData.artistChartItems.length;
+  },
+}
+</script>
 
 <style>
 #app {
--- src/views/ArtistDetails.vue
+++ src/views/ArtistDetails.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <b-container>
-      <b-img src="./../assets/nelly.png" height="150" />
-      <p>{{ id }}</p>
+      <b-img src="./../assets/nelly.png" height="150" class="m-3" />
+      <p style="border-top: 1px solid grey">
+        Artist Id: {{ id }}
+      </p>
     </b-container>
   </div>
 </template>
