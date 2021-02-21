c1a12e68e21988af2fc71b25936f81df7d619cd9
Stephen Anthony <stephenba4@gmail.com>
2021-02-21 12:19:11 -0600

    nb13: more artists

---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    id: 1,
+    artist: 'somegirlnamedanna',
+  },
+  {
+    id: 2,
+    artist: 'Quinn Lewis',
+  },
+  {
+    id: 3,
+    artist: 'Dylan Russell',
+  },
+  {
+    id: 4,
+    artist: 'Mokita',
+  },
+  {
+    id: 5,
+    artist: 'Callen',
+  },
+  {
+    id: 6,
+    artist: 'Jared Minnix',
+  },
+  {
+    id: 7,
+    artist: 'Drew Scheuler',
+  },
+  {
+    id: 8,
+    artist: 'Clinton John',
+  },
+  {
+    id: 9,
+    artist: 'Lost Stars',
+  },
+  {
+    id: 10,
+    artist: 'Kayla Nettles',
+  },
+  {
+    id: 11,
+    artist: 'Lackhoney',
+  },
+  {
+    id: 12,
+    artist: 'Brooke',
+  },
+  {
+    id: 13,
+    artist: 'Jon Santana',
+  },
+  {
+    id: 14,
+    artist: 'James Droll',
+  },
+  {
+    id: 15,
+    artist: 'Notelle',
+  },
+  {
+    id: 16,
+    artist: 'Bryce Green',
+  },
+  {
+    id: 17,
+    artist: 'Sansol the Artist',
+  },
+  {
+    id: 18,
+    artist: 'Abigail Rose',
+  },
+  {
+    id: 19,
+    artist: 'Talia Stewart',
+  },
+  {
+    id: 20,
+    artist: 'MILKK',
+  },
+  {
+    id: 21,
+    artist: 'Gatsby',
+  },
+]
