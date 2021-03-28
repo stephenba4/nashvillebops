3a7382a06a242a10d51b75fb5743066c7844b825
Stephen Anthony <stephenba4@gmail.com>
2021-03-28 00:49:03 -0500

    changed production to development when looking at env on digital ocean

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: (process.env.NODE_ENV === 'production')
+  baseURL: (process.env.NODE_ENV === 'development')
     ? 'https://nashvillebopsservice-hsft7.ondigitalocean.app/'
     : 'http://localhost:3000',
   headers: {
